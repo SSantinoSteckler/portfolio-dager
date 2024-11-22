953897f0f3229ebab726f5bc2d03e928490092c4
Agustin Gil <agustingil83@gmail.com>
2024-11-22 15:49:02 -0300

    updates

--- src/app/desktop/page.tsx
+++ src/app/desktop/page.tsx
@@ -1,27 +1,19 @@
-// import { Window } from '../components/Window';
+import { Window } from '../components/Window';
 import { DesktopView } from '../components/desktop/DesktopView';
 import { Footer } from '../components/desktop/Footer';
-<<<<<<< HEAD
 import { ProfileCard } from '../components/dotgpt/ProfileCard';
-=======
 import PlayList from '../components/playlist/PlayList';
->>>>>>> 3a0d167fc7e329603b8e107f7d1d45a50e8ab29b
 
 export default function DesktopPage() {
   return (
     <section className='bg-[#ddd8df] p-[24px] gap-5 flex flex-col h-screen '>
       <DesktopView>
-        <div>.</div>
-        {/* <Window name='dad'>
-          <span>POrfa anda</span>
-<<<<<<< HEAD
+        {/* <div>.</div> */}
+        <Window name='dad'>
+          <ProfileCard content='bdadkabdakdjbakdbkah dasjdkjhadkhakdhad dhaskdhakjhdkjah dajsdh' />
         </Window>
-        <ProfileCard content='ddddddddddddddddddddddddddddddddd djioajdoiajoidjoajdai sjodjaojdajdioajdojaid ijdoasjdoaijdiajiodj' />
-=======
-        </Window> */}
 
         <PlayList />
->>>>>>> 3a0d167fc7e329603b8e107f7d1d45a50e8ab29b
       </DesktopView>
       <Footer />
     </section>
